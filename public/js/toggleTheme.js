95556859869e678ba49383d19d7033cb9f489fe2
Leoff00 <leof0180@gmail.com>
2021-04-25 20:42:37 -0300

    fixing some bugs

--- public/js/toggleTheme.js
+++ public/js/toggleTheme.js
@@ -1,13 +1,13 @@
 function toggleTheme() {
-    $("#btn").click(() => {
-      $(".aboutContainer").toggleClass("darkCard");
-      $("body").toggleClass("darkBody");
-      $(".portfolioSecondPart").toggleClass("darkPortfolio");
-      $(".footerBox").toggleClass("darkFooter");
-      $(".navContainer").toggleClass("darkNav");
-      $(".navLinks").toggleClass("darkNavBtn");
-      $(".listContainer").toggleClass("darkList");
-    });
-  }
+  $("#btn").click(() => {
+    $(".aboutContainer").toggleClass("darkCard");
+    $("body").toggleClass("darkBody");
+    $(".portfolioSecondPart").toggleClass("darkPortfolio");
+    $(".footerBox").toggleClass("darkFooter");
+    $(".navContainer").toggleClass("darkNav");
+    $(".navLinks").toggleClass("darkNavBtn");
+    $(".listContainer").toggleClass("darkList");
+  });
+}
 
 toggleTheme();
